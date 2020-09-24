914cb2b3c76ea82e463b3ce1ab000cb1ace72a3b
Amimul <1805056@ugrad.cse.buet.ac.bd>
2020-09-24 12:26:09 +0600

    Shop added on admin.

--- app/src/main/java/com/owosuperadmin/owoshop/ManagementActivity.java
+++ app/src/main/java/com/owosuperadmin/owoshop/ManagementActivity.java
@@ -3,12 +3,30 @@ package com.owosuperadmin.owoshop;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.firebase.ui.database.paging.DatabasePagingOptions;
+import com.firebase.ui.database.paging.FirebaseRecyclerPagingAdapter;
+import com.firebase.ui.database.paging.LoadingState;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.owosuperadmin.model.PendingShop;
+import com.owosuperadmin.viewHolder.PendingShopViewHolderForAdmin;
+
 public class ManagementActivity extends AppCompatActivity {
+
+    FirebaseRecyclerPagingAdapter<PendingShop, PendingShopViewHolderForAdmin> adapter;
+
 
     private RecyclerView recyclerView;
     @Override
@@ -20,16 +38,56 @@ public class ManagementActivity extends AppCompatActivity {
 
         recyclerView=findViewById(R.id.management_recyclerviewid);
 
-        //here will be code for firebase recycler adapter and it will use management_sample.xml to inflate
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        Query baseQuery = databaseReference.child("approvedShops");
+
+        PagedList.Config config = new PagedList.Config.Builder()
+                .setEnablePlaceholders(false)
+                .setPrefetchDistance(10)
+                .setPageSize(20)
+                .build();
+
+        DatabasePagingOptions<PendingShop> options = new DatabasePagingOptions.Builder<PendingShop>()
+                .setLifecycleOwner(this)
+                .setQuery(baseQuery, config, PendingShop.class)
+                .build();
+
+
+        adapter = new FirebaseRecyclerPagingAdapter<PendingShop, PendingShopViewHolderForAdmin>(options) {
+                    @NonNull
+                    @Override
+                    public PendingShopViewHolderForAdmin onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                        View view = getLayoutInflater().inflate(R.layout.shops_sample_view, parent, false);
+                        return new PendingShopViewHolderForAdmin(view);
+                    }
+
+                    @Override
+                    protected void onBindViewHolder(@NonNull PendingShopViewHolderForAdmin holder, int position, @NonNull PendingShop model) {
+                        holder.bind(model, ManagementActivity.this);
+                    }
+
+                    @Override
+                    protected void onLoadingStateChanged(@NonNull LoadingState state) {
+
+                    }
+                };
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu,menu);
-
-        MenuItem item=menu.findItem(R.id.search);
-
-        SearchView searchView= (SearchView) item.getActionView();
-        return super.onCreateOptionsMenu(menu);
+    protected void onStart() {
+        super.onStart();
+        adapter.startListening();
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        adapter.stopListening();
+    }
+
 }
